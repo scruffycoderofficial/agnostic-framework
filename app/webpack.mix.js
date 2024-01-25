51c3223b1f9b65aa60885abb53d43bf3df0a1453
Luyanda Siko <sikoluyanda@gmail.com>
2024-01-25 18:31:55 +0200

    Added new styles for the login form and updated webpack.mix.js to include the file as part of the build process.

--- app/webpack.mix.js
+++ app/webpack.mix.js
@@ -3,4 +3,5 @@ let mix = require('laravel-mix');
 mix
     .js('resources/assets/js/app.js', 'js/app.js')
     .css('resources/assets/css/styles.css', 'css/app.css')
+    .css('resources/assets/css/views/auth.css', 'css/auth.css')
     .setPublicPath('public');

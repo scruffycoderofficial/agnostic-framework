e072fab9e2728fb3f2b73f9a6e65ae4db73620b2
Luyanda Siko <sikoluyanda@gmail.com>
2024-01-25 14:18:48 +0200

    Removed references to test controller and added a new AuthController to handle User authentication.

--- app/config/app.php
+++ app/config/app.php
@@ -10,12 +10,27 @@
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
-use D6\Invoice\App\Controller\LeapYearsController;
+use Psr\Log\LoggerInterface;
+use Doctrine\DBAL\Connection;
+use D6\Invoice\App\Auth\AuthService;
+use D6\Invoice\App\Controller\AuthController;
+use D6\Invoice\App\Repository\UserRepository;
+use D6\Invoice\App\Repository\UserRepositoryInterface;
 
 return static function (ContainerConfigurator $container): void {
     $services = $container->services();
 
-    $services->set(LeapYearsController::class)
+    $services->set(UserRepository::class)
+        ->arg('$connection', service(Connection::class));
+
+    $services->alias(UserRepositoryInterface::class, UserRepository::class);
+
+    $services->set(AuthService::class)
+        ->arg('$users', service(UserRepositoryInterface::class));
+
+    $services->set(AuthController::class)
+        ->arg('$authService', service(AuthService::class))
+        ->arg('$logger', service(LoggerInterface::class))
         ->tag('controller.service_arguments')
         ->public();
 };

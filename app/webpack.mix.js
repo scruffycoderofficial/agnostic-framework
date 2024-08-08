4262b4b26d9a17919fe2f308e5e35af6a8601096
Luyanda Siko <sikoluyanda@gmail.com>
2024-08-08 13:49:12 +0200

    Updated Webpack to work with TypeScript.

--- app/webpack.mix.js
+++ app/webpack.mix.js
@@ -1,12 +1,29 @@
-let mix = require('laravel-mix');
+const mix = require('laravel-mix');
 
-mix
-    .js('resources/assets/js/app.js', 'js/app.js')
-    .styles([
-            'resources/assets/css/styles.css'
-        ],
-        'css/app.css'
-    )
-    .css('resources/assets/css/views/auth.css', 'css/auth.css')
-    .options({ processCssUrls: false })
-    .setPublicPath('public');
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/assets/js/app.js', 'public/js/app.js')
+    .css('resources/assets/css/app.css', 'public/css/app.css')
+    .webpackConfig({
+        module: {
+            rules: [
+                {
+                    test:/\.tsx?$/,
+                    loader: "ts-loader",
+                    exclude: /node_modules/
+                }
+            ]
+        },
+        resolve: {
+            extensions: ["*", ".js", ".jsx", ".ts", ".tsx"]
+        }
+    });

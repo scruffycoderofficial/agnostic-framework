623225303a91b3419ca1d9effa8a99e4550afe0e
Luyanda Siko <sikoluyanda@gmail.com>
2024-08-08 13:04:20 +0200

    Updated routes by grouping them accordingly.

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * This file is part of the D6 Assessment Project.
+ * This file is part of the CoolStuff Enterprise Project.
  *
  * (c) Luyanda Siko <sikoluyanda@gmail.com>
  *
@@ -8,24 +8,82 @@
  * with this source code in the file LICENSE.
  */
 
+use CoolStuff\App;
 use Symfony\Component\Routing;
 
 $routes = new Routing\RouteCollection();
 
-$routes->add('login', new Routing\Route('/login', [
-    '_controller' => [D6\Invoice\App\Controller\AuthController::class, 'loginAction'],
+$routes->add('admin_dashboard', new Routing\Route('/admin/dashboard', [
+    '_controller' => [App\Admin\Controller\DashboardController::class, 'indexAction'],
 ]));
 
-$routes->add('logout', new Routing\Route('/logout', [
-    '_controller' => [D6\Invoice\App\Controller\AuthController::class, 'logoutAction'],
-]));
+/**
+ * Auth Routes.
+ */
+(function (Routing\RouteCollection $routes) {
+    $routes->add('login', new Routing\Route('/login', [
+        '_controller' => [App\Controller\AuthController::class, 'loginAction'],
+    ]));
 
-$routes->add('reports', new Routing\Route('/', [
-    '_controller' => [D6\Invoice\App\Controller\ReportsController::class, 'showAction'],
-]));
+    $routes->add('logout', new Routing\Route('/logout', [
+        '_controller' => [App\Controller\AuthController::class, 'logoutAction'],
+    ]));
+})($routes);
 
-$routes->add('reports_invoice', new Routing\Route('/reports/print/{userId}/{orderId}', [
-    '_controller' => [D6\Invoice\App\Controller\ReportsController::class, 'invoiceAction'],
-]));
+/**
+ * Products API Routes.
+ */
+(function (Routing\RouteCollection $routes) {
+    $routes->add('api_products_create', new Routing\Route('/v1/api/products/create', [
+        '_controller' => [CoolStuff\Api\Controller\ProductsController::class, 'createAction'],
+    ]));
+
+    $routes->add('api_products_list', new Routing\Route('/v1/api/products', [
+        '_controller' => [CoolStuff\Api\Controller\ProductsController::class, 'listAction'],
+    ]));
+})($routes);
+
+/**
+ * Products Admin Routes.
+ */
+(function (Routing\RouteCollection $routes) {
+    $routes->add('admin_products_list', new Routing\Route('/admin/products', [
+        '_controller' => [App\Controller\ProductsController::class, 'listAction'],
+    ]));
+
+    $routes->add('admin_products_create', new Routing\Route('/admin/products/create', [
+        '_controller' => [App\Controller\ProductsController::class, 'createAction'],
+    ]));
+
+    $routes->add('admin_products_update', new Routing\Route('/admin/products/{productId}/update', [
+        '_controller' => [App\Controller\ProductsController::class, 'updateAction'],
+    ]));
+
+    $routes->add('admin_products_view', new Routing\Route('/admin/products/{productId}/view', [
+        '_controller' => [App\Controller\ProductsController::class, 'viewAction'],
+        'method' => 'GET|POST',
+    ]));
+
+    $routes->add('admin_products_create_stock_journal', new Routing\Route('admin/journals/product/{productId}/create', [
+        '_controller' => [App\Controller\JournalsController::class, 'createJournalAction'],
+    ]));
+})($routes);
+
+/**
+ * Reports Routes.
+ */
+(function (Routing\RouteCollection $routes) {
+    $routes->add('reports', new Routing\Route('/admin/reports', [
+        '_controller' => [App\Controller\ReportsController::class, 'showAction'],
+    ]));
+
+    $routes->add('reports_update', new Routing\Route('/admin/reports/create', [
+        '_controller' => [App\Controller\ReportsController::class, 'updateAction'],
+    ]));
+
+    $routes->add('reports_invoice', new Routing\Route('/admin/reports/print/{userId}/{orderId}', [
+        '_controller' => [App\Controller\ReportsController::class, 'invoiceAction'],
+    ]));
+})($routes);
 
 return $routes;

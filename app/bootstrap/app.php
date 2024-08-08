0d6f3245090a752c7f638e4c2d7b01741993104f
Luyanda Siko <sikoluyanda@gmail.com>
2024-08-08 09:33:07 +0200

    Added few updates to ascertain that all configuration files are correctly loaded.

--- app/bootstrap/app.php
+++ app/bootstrap/app.php
@@ -9,36 +9,26 @@
  */
 
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use CoolStuff\App\CompilerPass\ProductsWebScraperCompilerPass;
 use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+use Craue\FormFlowBundle\DependencyInjection\CraueFormFlowExtension;
 use Symfony\Bridge\ProxyManager\LazyProxy\Instantiator\RuntimeInstantiator;
-use CoolStuff\App\DependencyInjection\CompilerPass\ProductsWebScraperCompilerPass;
+use Craue\FormFlowBundle\DependencyInjection\Compiler\LegacySessionCompilerPass;
 
 $container = new ContainerBuilder();
 
-/*
- * Sets ProxyManager to allow for managing Lazy services
- */
+$loader = new PhpFileLoader($container, new FileLocator(__DIR__.'/../config'));
+
 if (class_exists(RuntimeInstantiator::class) && method_exists($container, 'setProxyInstantiator')) {
     $container->setProxyInstantiator(new RuntimeInstantiator());
 }
 
-/**
- * Setup PHP File Loader.
- */
-$loader = new PhpFileLoader($container, new FileLocator(__DIR__.'/../config'));
-
 try {
-    /* Configuration parameters */
     $loader->load('parameters.php');
-
-    /* Application business configurations */
-    $loader->load('products.php');
-
-    /*
-     * External service configurations
-     */
-    $loader->load('goutte.php');
+    $loader->load('console.php');
+    $loader->load('doctrine.php');
     $loader->load('monolog.php');
 
     /* Core framework configurations */
@@ -48,25 +38,41 @@ try {
 
     /* Application framework configurations */
     $loader->load('app.php');
+    $loader->load('mail.php');
     $loader->load('console.php');
-
+    $loader->load('goutte.php');
+    $loader->load('events.php');
     $loader->load('forms.php');
     $loader->load('views.php');
     $loader->load('dompdf.php');
     $loader->load('doctrine.php');
     $loader->load('migrations.php');
     $loader->load('fixtures.php');
+    $loader->load('security.php');
+
+    $loader->load('products.php');
+    $loader->load('repositories.php');
+    $loader->load('services.php');
+    $loader->load('controllers.php');
 } catch (Exception $exc) {
 } finally {
-    /* Add CompilerPassInterface definitions */
     $container->addCompilerPass(new ProductsWebScraperCompilerPass());
 
-    /*
-     * Compile Container, if not compiled already
-     */
-    if (! $container->isCompiled()) {
-        $container->compile();
+    if (! \method_exists(RequestStack::class, 'getSession')) {
+        $container->addCompilerPass(new LegacySessionCompilerPass());
     }
+
+    /**
+    $container->registerExtension((function () use ($container) {
+        $craueFormFlowExtension = new CraueFormFlowExtension();
+        $craueFormFlowExtension->load([], $container);
+
+        return $craueFormFlowExtension;
+    })());*/
+}
+
+if (! $container->isCompiled()) {
+    $container->compile(true);
 }
 
 return $container;
